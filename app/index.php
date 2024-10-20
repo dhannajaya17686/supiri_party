3af012be144335de997813baa9615be7a014fb60
dhannajaya17686 <dananjayamudalige@gmail.com>
2024-10-20 08:06:52 +0530

    Dashboard ui COmpleted

--- app/index.php
+++ app/index.php
@@ -6,61 +6,43 @@
         <!--The component to greet the user-->    
         <?php require_once("../components/stat-box.php"); ?>
         <!--The component to show to upcoming parties-->
-        <div class="upcoming-parties-admin-box">
+        <?php require_once("../components/upcoming-parties-admin.php");?>
+        <!--The componnet for upcoming tasks-->
+        <div class="assigned-tasks-admin-box">
             <div class="action-row">           
-                <h3>Upcoming Parties</h3>
-                <button class="details-btn">Show more</button>
-            </div>       
-            <table class="party-table">
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>Party Name</th>
-                        <th>Party Type</th>
-                        <th>Customer</th>
-                        <th>Remaing Time</th>
-                        <th>Status</th>
-                        <th>Balance</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>01</td>
-                        <td>Sudage Party</td>
-                        <th>Birthday</th>
-                        <td><img src="../assets/icons/flash-circle.png" alt="Alex Noman" class="customer-img" />Alex Noman</td>
-                        <td>1 days </td>
-                        <td><span class="status completed"></span> Completed</td>
-                        <td>$ 35.44</td>
-                        <td><button class="details-btn">Details</button></td>
-                    </tr>
-                    <tr>
-                                <td>02</td>
-                                <td>Sudage Party</td>
-                                <th>Birthday</th>
-                                <td><img src="../assets/icons/flash-circle.png" alt="Razib Rahman" class="customer-img" />Razib Rahman</td>
-                                <td>2 days</td>
-                                <td><span class="status pending"></span> Pending</td>
-                                <td>$ 0.00</td>
-                                <td><button class="details-btn">Details</button></td>
-                    </tr>
-                    <tr>
-                                <td>02</td>
-                                <td>Sudage Party</td>
-                                <th>Birthday</th>
-                                <td>
-                                <img src="../assets/icons/flash-circle.png" alt="Razib Rahman" class="customer-img" />
-                                Razib Rahman
-                                </td>
-                                <td>4 hours</td>
-                                <td><span class="status pending"></span> Pending</td>
-                                <td>$ 0.00</td>
-                                <td><button class="details-btn">Details</button></td>
-                    </tr>            
-                </tbody>    
-            </table>
-
+                <h3>Sunday 23 May</h3>
+                <button class="details-btn">Add new tasks</button>
+            </div>
+            <div class="task-list">            
+                <div class="task-box">
+                    <p class="title">Meet M/W Simmple</p>
+                    <div class="detail-row">
+                        <p class="status">Completed</p>
+                        <p class="assigned-to">Assigned to: John</p>
+                    </div>
+                </div>
+                <div class="task-box">
+                    <p class="title">Meet M/W Simmple</p>
+                    <div class="detail-row">
+                        <p class="status">Completed</p>
+                        <p class="assigned-to">Assigned to: John</p>
+                    </div>
+                </div>
+                <div class="task-box">
+                    <p class="title">Meet M/W Simmple</p>
+                    <div class="detail-row">
+                        <p class="status">Completed</p>
+                        <p class="assigned-to">Assigned to: John</p>
+                    </div>
+                </div>
+                <div class="task-box">
+                    <p class="title">Meet M/W Simmple</p>
+                    <div class="detail-row">
+                        <p class="status">Completed</p>
+                        <p class="assigned-to">Assigned to: John</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     
---
+++ components/upcoming-parties-admin.php
@@ -0,0 +1,56 @@
+<div class="upcoming-parties-admin-box">
+            <div class="action-row">           
+                <h3>Upcoming Parties</h3>
+                <button class="details-btn">Show more</button>
+            </div>       
+            <table class="party-table">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Party Name</th>
+                        <th>Party Type</th>
+                        <th>Customer</th>
+                        <th>Remaing Time</th>
+                        <th>Status</th>
+                        <th>Balance</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>01</td>
+                        <td>Sudage Party</td>
+                        <th>Birthday</th>
+                        <td><img src="../assets/icons/flash-circle.png" alt="Alex Noman" class="customer-img" />Alex Noman</td>
+                        <td>1 days </td>
+                        <td><span class="status completed"></span> Completed</td>
+                        <td>$ 35.44</td>
+                        <td><button class="details-btn">Details</button></td>
+                    </tr>
+                    <tr>
+                                <td>02</td>
+                                <td>Sudage Party</td>
+                                <th>Birthday</th>
+                                <td><img src="../assets/icons/flash-circle.png" alt="Razib Rahman" class="customer-img" />Razib Rahman</td>
+                                <td>2 days</td>
+                                <td><span class="status pending"></span> Pending</td>
+                                <td>$ 0.00</td>
+                                <td><button class="details-btn">Details</button></td>
+                    </tr>
+                    <tr>
+                                <td>02</td>
+                                <td>Sudage Party</td>
+                                <th>Birthday</th>
+                                <td>
+                                <img src="../assets/icons/flash-circle.png" alt="Razib Rahman" class="customer-img" />
+                                Razib Rahman
+                                </td>
+                                <td>4 hours</td>
+                                <td><span class="status pending"></span> Pending</td>
+                                <td>$ 0.00</td>
+                                <td><button class="details-btn">Details</button></td>
+                    </tr>            
+                </tbody>    
+            </table>
+
+        </div>
